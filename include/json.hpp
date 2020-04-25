c20929ce45e057a5d3b9cba11c97e27f8fe53814
Elviss Strazdins <elviss@elviss.lv>
2020-04-25 15:06:33 +0300

    Encode to std::string

--- include/json.hpp
+++ include/json.hpp
@@ -39,7 +39,7 @@ namespace json
     {
         constexpr std::uint8_t UTF8_BOM[] = {0xEF, 0xBB, 0xBF};
 
-        inline void encodeString(std::vector<std::uint8_t>& data,
+        inline void encodeString(std::string& data,
                                  const std::string& str)
         {
             for (const auto c : str)
@@ -58,10 +58,10 @@ namespace json
 
                     constexpr char digits[] = "0123456789abcdef";
                     for (std::uint32_t p = 0; p < 4; ++p)
-                        data.push_back(static_cast<std::uint8_t>(digits[(c >> (12 - p * 4)) & 0x0F]));
+                        data.push_back(digits[(c >> (12 - p * 4)) & 0x0F]);
                 }
                 else
-                    data.push_back(static_cast<std::uint8_t>(c));
+                    data.push_back(c);
             }
         }
 
@@ -698,7 +698,7 @@ namespace json
             type = Type::Array;
         }
 
-        void encodeValue(std::vector<std::uint8_t>& data) const
+        void encodeValue(std::string& data) const
         {
             switch (type)
             {
@@ -805,9 +805,9 @@ namespace json
             parseValue(iterator, tokens.end());
         }
 
-        std::vector<std::uint8_t> encode() const
+        std::string encode() const
         {
-            std::vector<std::uint8_t> result;
+            std::string result;
 
             if (bom) result.assign(std::begin(UTF8_BOM), std::end(UTF8_BOM));
 
