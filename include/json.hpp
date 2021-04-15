bef2e503262f3a259ece1892dcf9e8a49c0df0d6
Elviss Strazdins <elviss@elviss.lv>
2021-04-15 03:18:19 +0300

    Use init statement for endIterator

--- include/json.hpp
+++ include/json.hpp
@@ -314,8 +314,7 @@ namespace json
             {
                 const auto startIterator = hasByteOrderMark(begin, end) ? begin + 3 : begin;
                 const auto [result, valueIterator] = parseValue(startIterator, end);
-                const auto endIterator = skipWhitespaces(valueIterator, end);
-                if (endIterator != end)
+                if (const auto endIterator = skipWhitespaces(valueIterator, end); endIterator != end)
                     throw ParseError("Unexpected data");
 
                 return result;
