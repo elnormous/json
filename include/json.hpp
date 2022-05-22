7f7346515ce0719cbb23bfa1d45499fd0f40dcbd
Elviss Strazdins <elviss@elviss.lv>
2022-05-22 18:45:21 +0300

    Use strlen

--- include/json.hpp
+++ include/json.hpp
@@ -710,9 +710,7 @@ namespace json
 
     [[nodiscard]] inline Value parse(const char* data)
     {
-        auto end = data;
-        while (*end) ++end;
-        return parse(data, end);
+        return parse(data, data + std::strlen(data));
     }
 
     template <class Data>
